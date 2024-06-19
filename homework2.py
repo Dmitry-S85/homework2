c8d95f8a512d64b63dca291edc4667ccce4fbc23
Dmitry-S85 <dmitry.stoyankin@yandex.ru>
2024-06-19 22:53:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,13 @@
+#Вариант № 1:
+Number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = (int(Number_of_completed_homework_assignments / Number_of_hours_spent))
+print('Course: ', Course_name,',', ' total tasks: ', Number_of_completed_homework_assignments,',', 'hours spent: ', Number_of_hours_spent,',', 'Time for one task: ', Time_for_one_task, ' hours')
+
+#Вариант № 2:
+Количество_выполненных_ДЗ = 12
+Количество_затраченных_часов = 1.5
+Название_курса = 'Python'
+Время_на_одно_задание = (int(Количество_выполненных_ДЗ / Количество_затраченных_часов))
+print('Курс: ', Название_курса,',','всего задач: ', Количество_выполненных_ДЗ,',','затрачено часов: ',Количество_затраченных_часов,',', 'среднее время выполнения: ', Время_на_одно_задание, 'часов')
